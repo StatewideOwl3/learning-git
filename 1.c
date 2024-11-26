2d8366ad3d0ad61cd45d362233db7347d158b078
bonda <sai.yeddanapudi2205@gmail.com>
2024-11-26 17:52:24 +0530

    comming all... even vscode json kinda scary

--- 1.c
+++ 1.c
@@ -3,5 +3,8 @@
 int main(){
   printf("this is my first time using git!\n");
   printf("i am adding this new line!\n");
+  printf("git commit -a -m 'message'  to skip staging\n");
+  printf("git diff command!\n");
+
   return 0;
 }
---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is my first time using Git
+User: BONDA
+Email: sai.yeddanapudi2205@gmail.com
